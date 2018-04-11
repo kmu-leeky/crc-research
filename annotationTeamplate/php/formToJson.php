846788fa371396f04e8e86d37602308c12feed6d
Ubuntu <ubuntu@ip-172-31-31-202.ap-northeast-2.compute.internal>
2018-04-11 00:59:22 +0000

    정리되지 않은 Template

---
+++ annotationTeamplate/php/formToJson.php
@@ -0,0 +1,22 @@
+<?php
+$getData = $_POST["data"];
+$getTag =  $_POST["tag"];
+
+if(!empty($getData)){
+	
+	//echo $getData.$getTag;
+	$savePath = "/var/www/html/jsondata/";
+	$fileName = $savePath.$getTag.".json";
+	if(file_exists($fileName)){
+		echo "ex";
+		$fp = fopen($fileName, 'a');
+	} else {
+		echo "no";
+		$fp = fopen($fileName, 'w');
+	}
+	fwrite($fp, $getData);
+	fclose($fp);
+
+}
+clearstatcache();	
+?>
